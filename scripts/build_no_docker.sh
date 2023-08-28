3c06f1f30bf061d2cc55fe519b38957e05e37873
tarolling <benitojoens@gmail.com>
2023-08-28 15:28:44 -0400

    Oops typo

--- scripts/build_no_docker.sh
+++ scripts/build_no_docker.sh
@@ -2,7 +2,7 @@
 sudo apt-get update -y && \
 apt-get install --no-install-recommends -y libssl-dev zlib1g-dev libsodium-dev libopus-dev
 
-sudo wget wget -O dpp.deb https://dl.dpp.dev/
+sudo wget -O dpp.deb https://dl.dpp.dev/
 sudo dpkg -i dpp.deb
 rm dpp.deb
 
