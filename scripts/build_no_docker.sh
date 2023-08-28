9e3f27f8e80969aa255c824fb939e8a66f494e8b
tarolling <benitojoens@gmail.com>
2023-08-28 15:26:10 -0400

    Ok it will pass this time

--- scripts/build_no_docker.sh
+++ scripts/build_no_docker.sh
@@ -3,7 +3,7 @@ sudo apt-get update -y && \
 apt-get install --no-install-recommends -y libssl-dev zlib1g-dev libsodium-dev libopus-dev
 
 sudo wget wget -O dpp.deb https://dl.dpp.dev/
-dpkg -i dpp.deb
+sudo dpkg -i dpp.deb
 rm dpp.deb
 
 mkdir build && cd build
