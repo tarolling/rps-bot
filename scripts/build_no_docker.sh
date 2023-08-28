7938bbfabd0d331bfd22b51164cb4a3148d580fc
tarolling <benitojoens@gmail.com>
2023-08-28 15:23:14 -0400

    Non-sudo mode?

--- scripts/build_no_docker.sh
+++ scripts/build_no_docker.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
+apt-get update -y && \
 apt-get install --no-install-recommends -y libssl-dev zlib1g-dev libsodium-dev libopus-dev
 
 wget wget -O dpp.deb https://dl.dpp.dev/
-sudo dpkg -i dpp.deb
+dpkg -i dpp.deb
 rm dpp.deb
 
 mkdir build && cd build
