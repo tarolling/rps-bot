70f95b1a3ff6f1b997a15632da0842f876698186
tarolling <benitojoens@gmail.com>
2023-08-28 15:31:36 -0400

    please work

--- scripts/build_no_docker.sh
+++ scripts/build_no_docker.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 sudo apt-get update -y && \
-apt-get install --no-install-recommends -y libssl-dev zlib1g-dev libsodium-dev libopus-dev
+sudo apt-get install --no-install-recommends -y libssl-dev zlib1g-dev libsodium-dev libopus-dev
 
 sudo wget -O dpp.deb https://dl.dpp.dev/
-sudo dpkg -i dpp.deb
+sudo -s dpkg -i dpp.deb
 rm dpp.deb
 
 mkdir build && cd build
