261d7d191acf3d9707b733759287b8af661d1fe0
tarolling <benitojoens@gmail.com>
2023-08-29 13:46:19 -0400

    Trying again

--- scripts/build_no_docker.sh
+++ scripts/build_no_docker.sh
@@ -2,7 +2,7 @@
 sudo apt-get update -y && \
 sudo apt-get install --no-install-recommends -y libssl-dev zlib1g-dev libsodium-dev libopus-dev
 
-sudo -s dpkg -i dpp.deb
+sudo -s dpkg -i libs/dpp.deb
 
 mkdir build && cd build || exit
 cmake -S ../ -B .
