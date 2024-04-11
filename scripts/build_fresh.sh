d86b87e32261fa0beceaff53a5acf4e71e9b2d11
tarolling <benitojoens@gmail.com>
2024-04-10 23:57:00 -0500

    Pin version in bash script

--- scripts/build_fresh.sh
+++ scripts/build_fresh.sh
@@ -5,7 +5,7 @@ sudo apt-get update -y && \
 sudo apt-get install --no-install-recommends -y libssl-dev zlib1g-dev libsodium-dev libopus-dev libspdlog-dev=1.13.0 libfmt-dev=8.1.1 cmake pkg-config g++ gcc git make && \
 sudo apt-get clean
 # Install DPP dependency
-wget -O dpp.deb https://dl.dpp.dev/
+wget -O dpp.deb https://github.com/brainboxdotcc/DPP/releases/download/v10.0.29/libdpp-10.0.29-linux-x64.deb
 sudo -s dpkg -i dpp.deb
 rm dpp.deb
 
