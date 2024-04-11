4d6819e9055c034949ce8313bb499a400ce782bf
tarolling <benitojoens@gmail.com>
2024-04-10 23:59:58 -0500

    Pinning everything but my own dev env to Ubuntu 20.04

--- scripts/build_fresh.sh
+++ scripts/build_fresh.sh
@@ -2,7 +2,7 @@
 
 # Install packages
 sudo apt-get update -y && \
-sudo apt-get install --no-install-recommends -y libssl-dev zlib1g-dev libsodium-dev libopus-dev libspdlog-dev=1.13.0 libfmt-dev=8.1.1 cmake pkg-config g++ gcc git make && \
+sudo apt-get install --no-install-recommends -y libssl-dev zlib1g-dev libsodium-dev libopus-dev libspdlog-dev=1:1.5.0-1 libfmt-dev=6.1.2+ds-2 cmake pkg-config g++ gcc git make && \
 sudo apt-get clean
 # Install DPP dependency
 wget -O dpp.deb https://github.com/brainboxdotcc/DPP/releases/download/v10.0.29/libdpp-10.0.29-linux-x64.deb
