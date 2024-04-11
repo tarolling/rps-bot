3c5cb53c98cc885c8bd6af4aa856fc870bf8e0f7
tarolling <benitojoens@gmail.com>
2024-04-10 23:08:05 -0500

    Fix codeql build

--- scripts/build_no_docker.sh
+++ scripts/build_no_docker.sh
@@ -1,15 +1,5 @@
 #!/bin/sh
 
-# UNCOMMENT IF RUNNING FOR THE FIRST TIME
-# # Install packages
-# sudo apt-get update -y && \
-# sudo apt-get install --no-install-recommends -y libssl-dev zlib1g-dev libsodium-dev libopus-dev libspdlog-dev cmake pkg-config g++ gcc git make && \
-# sudo apt-get clean
-# # Install DPP dependency
-# wget -O dpp.deb https://dl.dpp.dev/
-# sudo -s dpkg -i dpp.deb
-# rm dpp.deb
-
 rm -rf build/
 mkdir build && cd build || exit
 cmake -S ../ -B .
