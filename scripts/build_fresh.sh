50dd8bba492df629ee56ea3497b47cd1c9f7b372
tarolling <benitojoens@gmail.com>
2024-04-11 22:36:59 -0500

    how about this...

--- scripts/build_fresh.sh
+++ scripts/build_fresh.sh
@@ -4,10 +4,9 @@
 sudo apt-get update -y && \
 sudo apt-get install --no-install-recommends -y sudo gcc g++ build-essential git cmake libssl-dev libspdlog-dev=1:1.5.0-1 zlib1g-dev curl wget libpcre3-dev libsodium23 libopus0 nlohmann-json3-dev libfmt-dev=6.1.2+ds-2 libmysqlclient-dev && \
 sudo apt-get clean && \
-rm -rf /var/lib/apt/lists/*
+sudo rm -rf /var/lib/apt/lists/*
 # Symlink weirdness
-ln -s libssl.so.3 libssl.so
-sudo ldconfig
+sudo ldconfig /x86_64-linux-gnu/
 # Install DPP dependency
 wget -O dpp.deb https://github.com/brainboxdotcc/DPP/releases/download/v10.0.29/libdpp-10.0.29-linux-x64.deb
 sudo -s dpkg -i dpp.deb
