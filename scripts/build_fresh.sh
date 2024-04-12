df08cd741632075ed718be43577b090dc1c2ae49
tarolling <benitojoens@gmail.com>
2024-04-11 22:21:49 -0500

    how ab now

--- scripts/build_fresh.sh
+++ scripts/build_fresh.sh
@@ -1,12 +1,10 @@
 #!/bin/sh
 
 # Install packages
-sudo apt-get update -y
-sudo apt-get install -y sudo gcc g++ build-essential git cmake libssl-dev libspdlog-dev=1:1.5.0-1 zlib1g-dev curl wget libpcre3-dev libsodium23 libopus0 nlohmann-json3-dev libfmt-dev=6.1.2+ds-2 libmysqlclient-dev
-sudo apt-get clean
+sudo apt-get update -y && \
+sudo apt-get install --no-install-recommends -y sudo gcc g++ build-essential git cmake libssl-dev libspdlog-dev=1:1.5.0-1 zlib1g-dev curl wget libpcre3-dev libsodium23 libopus0 nlohmann-json3-dev libfmt-dev=6.1.2+ds-2 libmysqlclient-dev && \
+sudo apt-get clean && \
 rm -rf /var/lib/apt/lists/*
-sudo apt-get install --no-install-recommends -y sudo gcc g++ build-essential git cmake libssl-dev zlib1g-dev libsodium-dev libopus-dev libspdlog-dev=1:1.5.0-1 libfmt-dev=6.1.2+ds-2 lohmann-json3-dev pkg-config make && \
-sudo apt-get clean
 # Symlink weirdness
 ln -s libssl.so.3 libssl.so
 sudo ldconfig
